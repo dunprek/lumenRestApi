bb2d59cf335b6caf54e401ec5a5d97e860701242
Gideon <gideon.tobing@Hotmail.com>
2017-12-14 11:20:21 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+//get all user defined views
+$app->get('user_views/{id}', 'UserViewsController@getAllUserViews');
+
+//create user view
+$app->post('createUserView', 'UserViewsController@createUserView');
+
+//get user view by ID
+$app->get('user_view/{id}', 'UserViewsController@getUserViewById');
+
+//update user view
+$app->post('saveUserView', 'UserViewsController@saveUserView');
+
+//delete user view
+$app->delete('delete_view/{id}', 'UserViewsController@deleteUserView');
